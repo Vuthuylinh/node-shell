a4559ca677917f398327209f59a7f0145f435c89
Linh Vu <thuylinh2490@gmail.com>
2020-11-13 15:19:15 -0500

    updated cwd

--- bash.js
+++ bash.js
@@ -1,7 +1,10 @@
 process.stdout.write("prompt > ");
 process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
-
-  process.stdout.write("You typed: " + cmd);
+  if(cmd==='pwd'){
+    console.log(process.cwd())
+  }else{
+    process.stdout.write("You typed: " + cmd);
+  }
   process.stdout.write("\nprompt > ");
 });
